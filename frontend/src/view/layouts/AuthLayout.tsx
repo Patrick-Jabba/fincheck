b40b43a13f6fee2d728b6adeef2e32aa9a73192c
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2024-07-28 16:24:31 -0300

    start do frontend

---
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -0,0 +1,8 @@
+export function AuthLayout(){
+  return(
+    <div className="flex w-full h-full">
+      <div className="w-1/2 h-full"></div>
+      <div className="w-1/2 h-full"></div>
+    </div>
+);
+}
